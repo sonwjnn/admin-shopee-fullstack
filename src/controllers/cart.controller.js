36ec7a6eb088b9917a84a07e1ca68394971e0518
sonwjnn <hoangsonj4f@gmail.com>
2023-08-05 16:38:27 +0700

    created pagination

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,9 +1,6 @@
-const express = require('express')
-const router = express.Router()
 const responseHandler = require('../handlers/response.handler.js')
 
 const cartModel = require('../models/cart.model.js')
-const userModel = require('../models/user.model.js')
 
 const getCartsOfUser = async (req, res) => {
   try {
@@ -78,77 +75,5 @@ const removeCarts = async (req, res) => {
     responseHandler.error(res)
   }
 }
-
-// router.post('/updateStatus', function (req, res) {
-//   var status = (idUser = token = address = phone = '')
-//   var flagError = 0
-//   /*  var arr = JSON.parse(req.body.arr); */
-
-//   var arr = JSON.parse(req.body.arr)
-//   token = req.body.token
-//   phone = req.body.phone
-//   address = req.body.address
-//   status = 'done'
-
-//   if (token == '' || token == undefined) {
-//     res.send({ kq: 0, msg: 'None user' })
-//   } else {
-//     jwt.verify(token, secret, function (err, decoded) {
-//       if (err) {
-//         // token expired!
-//         res.send({ kq: 0, msg: 'token expired' })
-//       } else {
-//         if (decoded.data != '') {
-//           idUser = decoded.data
-
-//           for (var i = 0; i < arr.arr.length; i++) {
-//             const obj = { phone, address, status }
-
-//             cartModel.updateMany(
-//               { idUser: idUser, idProduct: arr.arr[i], status: 'ready' },
-//               obj,
-//               (err, data) => {
-//                 if (err) {
-//                   flagError = 1
-//                   throw err
-//                 }
-//               }
-//             )
-//           }
-
-//           if (flagError == 0) {
-//             res.send({ kq: 1, msg: 'Update data successfully' })
-//           } else {
-//             res.send({ kq: 0, msg: 'Error DB' })
-//           }
-//         } else {
-//           res.send({ kq: 0, msg: 'No data!' })
-//         }
-//       }
-//     })
-//   }
-// })
-
-// router.post('/delete', function (req, res) {
-//   var idUser = req.body.idUser
-//   var idProduct = req.body.idProduct
-
-//   const check_obj = { idUser, idProduct }
-//   cartModel.find(check_obj).exec((err, data) => {
-//     if (err) {
-//       res.send({ kq: 0, msg: 'Connection to database failed' })
-//     }
-
-//     if (data == '') {
-//       res.send({ kq: 0, msg: 'Data id not exists' })
-//     } else {
-//       cartModel.findByIdAndDelete({ _id: data[0]._id }, (err, data) => {
-//         if (err) {
-//           res.send({ kq: 0, msg: 'Connection to database failed' })
-//         } else res.send({ kq: 1, msg: 'Delete data successfully!' })
-//       })
-//     }
-//   })
-// })
 
 module.exports = { getCartsOfUser, addCart, removeCart, removeCarts }
