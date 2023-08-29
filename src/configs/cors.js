89f9bcae1f9bf2487ef09af0240850990d3156c7
sonwjnn <hoangsonj4f@gmail.com>
2023-08-29 16:36:06 +0700

    fix cors

--- src/configs/cors.js
+++ src/configs/cors.js
@@ -1,14 +1,13 @@
-const { WHITELIST_DOMAINS } = require("../utilities/constants.js");
+const { WHITELIST_DOMAINS } = require('../utilities/constants.js')
 
 const corsOptions = {
   origin: function (origin, callback) {
-    console.log(WHITELIST_DOMAINS.indexOf(origin));
-    if (origin && WHITELIST_DOMAINS.indexOf(origin) !== -1) {
-      callback(null, true);
+    if (origin && WHITELIST_DOMAINS.indexOf("'" + origin + "'") !== -1) {
+      callback(null, true)
     } else {
-      callback(new Error(`${origin} not allowed by CORS.`));
+      callback(new Error(`${origin} not allowed by CORS.`))
     }
-  },
-};
+  }
+}
 
-module.exports = { corsOptions };
+module.exports = { corsOptions }
